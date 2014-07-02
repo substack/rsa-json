6b99061c8b86fd8bf0c97b3f9b8076fec80c1e1b
Forbes Lindesay <forbes@lindesay.co.uk>
2014-07-02 13:39:55 +0100

    Make browser API match server side API

--- browser.js
+++ browser.js
@@ -1,1 +1,18 @@
-module.exports = require('keypair');
+var keypair = require('keypair');
+
+module.exports = function (opts, cb) {
+    if (typeof opts === 'function') {
+        cb = opts;
+        opts = {};
+    }
+    setImmediate(function () {
+        var result;
+        try {
+            result = keypair(opts);
+        } catch (ex) {
+            return cb(ex);
+        }
+        cb(null, result);
+    });
+};
+module.exports.native = keypair;
